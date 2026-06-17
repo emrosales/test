1b6bfc9e0d34282e45e51ee9ef93e5e49a878ea2
Eric Rosales <ericmrp@hotmail.com>
2026-06-16 21:56:18 -0600

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,5 @@
-print("I'm hungry")
+hungry=input("Are you hungry? (yes/no) ")
+if hungry.lower() == "yes" or hungry.lower() == "y":
+    print("You should eat something!")
+else:
+    print("Do your homework or go outside and play!")
